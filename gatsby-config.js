b7785de0a74a92e9061759ff386f133658f316fb
ryanolee <rizza@rizza.net>
2022-10-23 00:45:00 +0100

    Add some dogs 🐩

---
+++ gatsby-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    siteMetadata: {
+        title: `Dog blog 🐶`,
+        author: "a-normal-dog"
+    },
+    pathPrefix: "/dog-blog",
+    plugins: ["gatsby-theme-gallery"],
+};
